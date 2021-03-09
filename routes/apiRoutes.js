67da29ecac6c6870a50b9f4bf3e7ade15c9855c3
flokamp <74511633+flokamp@users.noreply.github.com>
2021-03-08 19:33:49 -0600

    Add Heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,7 +10,11 @@ router.get('/notes', (req, res)=>{
 // push to notes array and json file
 router.post('/notes', (req, res) => {
   const note = req.body;
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
   notes.push(note);
+  
   fs.writeFileSync(
     path.join(__dirname, '../db/db.json'),
     JSON.stringify({notes: notes}, null, 2)
