825327e4bee9696789edc5944e08f2a6dae1d5e4
flokamp <74511633+flokamp@users.noreply.github.com>
2021-03-10 09:02:13 -0600

    unique id package

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,46 +1,37 @@
-
 const router = require('express').Router();
 let { notes } = require('../db/db.json');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res)=>{
   res.json(notes)
 })
 
-router.get('/notes/:id', (req, res) => {
-  const result = notes.find(note => note.id === id);
-  if (result) {
-    res.json(result);
-    return result;
-  } else {
-    res.send(404);
-  }
-});
-
 // push to notes array and json file
 router.post('/notes', (req, res) => {
   const note = req.body;
 
-  if (req.body.id !== undefined) {
-    console.log(req.body.id)
-    notes.map(note);
-  } else {
-  // set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
-  notes.push(note);
 
+  
+  // note.id = req.body.id === true ? req.body.id : notes.length.toString();
+  if (req.body.id) {
+    // find that note in the notes array using the id
+    const { id } = req.body;
+    let existing = notes.findIndex(note => note.id === id)
+    console.log(existing)
+    notes[existing] = note;
+  
+  } else {
+    note.id = uuidv4();
+    notes.push(note);
+  }
   fs.writeFileSync(
     path.join(__dirname, '../db/db.json'),
     JSON.stringify({notes: notes}, null, 2)
   );
   res.json(note)
-  }
 });
-
-
-
-
 
 // delete note
 router.delete('/notes/:id', (req, res) => {
